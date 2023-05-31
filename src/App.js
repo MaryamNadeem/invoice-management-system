df32ee21a25fd48426b8df6389b7341184de9824
Maryam <maryam.nadeem19@gmail.com>
2023-05-31 13:37:44 +0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// @import modules
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// @import components
+import InvoiceForm from "./components/InvoiceForm";
+import CustomerForm from "./components/CustomerForm";
+import InvoiceListing from "./components/InvoiceListing";
+import CustomerListing from "./components/CustomerListing";
+// @import modules
+import Navbar from "./modules/Navbar";
+
+const routes = [
+  {
+    path: "/",
+    component: <InvoiceListing />,
+  },
+  {
+    path: "/new-invoice",
+    component: <InvoiceForm />,
+  },
+  {
+    path: "/new-customer",
+    component: <CustomerForm />,
+  },
+  {
+    path: "/customer-listing",
+    component: <CustomerListing />,
+  },
+];
 
 function App() {
+  const routeComponents = routes.map(({ path, component }, key) => (
+    <Route exact path={path} element={component} key={key} />
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>{routeComponents}</Routes>
+      </BrowserRouter>
     </div>
   );
 }
